b7cdd9f9aa654c72050dded8cbc11be8a2f9b9ec
Christopher <christopher.black.2209@googlemail.com>
2021-11-01 23:27:07 +0000

    accepts nested attributes added to Parent

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -3,4 +3,5 @@ class Parent < ApplicationRecord
 
     has_many :children
     has_many :childrens_appointments, through: :children, source: :appointments
+    accepts_nested_attributes_for :children
 end
