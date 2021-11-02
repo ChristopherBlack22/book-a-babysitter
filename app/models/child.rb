d6c87cd4aee9938a2bba31bdc7da8a0b5f276795
Christopher <christopher.black.2209@googlemail.com>
2021-11-02 01:28:43 +0000

    Validations added to Child

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,4 +2,6 @@ class Child < ApplicationRecord
     belongs_to :parent
     has_many :appointments
     has_many :babysitters, through: :appointments
+
+    validates :name, :age, presence: true
 end
