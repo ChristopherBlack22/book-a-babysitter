c523ee22eb70a9769215e4defea65839fe6d2347
Christopher <christopher.black.2209@googlemail.com>
2021-11-02 01:07:51 +0000

    helper methods added to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        current_user = Parent.find_by(id: session[:user_id])
+    end 
+
+    def logged_in?
+        #!!session[:user_id] #turning the value if present into a statement of truthiness
+        if current_user
+        end 
+    end 
+
+    def redirect_if_not_logged_in
+        redirect_to "/" unless logged_in?
+    end 
 end
