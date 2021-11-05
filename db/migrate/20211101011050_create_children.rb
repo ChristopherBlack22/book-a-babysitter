a78a3be72865595f84748308d4bef7b9052cdc75
Christopher <christopher.black.2209@googlemail.com>
2021-11-05 14:02:48 +0000

    changes to Child and seed data

--- db/migrate/20211101011050_create_children.rb
+++ db/migrate/20211101011050_create_children.rb
@@ -2,7 +2,7 @@ class CreateChildren < ActiveRecord::Migration[6.1]
   def change
     create_table :children do |t|
       t.string :name
-      t.integer :age
+      t.date :date_of_birth
       t.integer :parent_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_012322) do
-
-  create_table "babysitters", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.string "address"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "bookings", force: :cascade do |t|
-    t.integer "child_id"
-    t.integer "babysitter_id"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer "rating"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "children", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.integer "parent_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "parents", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
